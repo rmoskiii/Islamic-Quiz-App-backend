594daec4e79dce9cb50ee079a45b10fe6938e832
rmoskiii <rmagnus008@yahoo.com>
2025-06-05 17:05:49 +0100

    Frontend basic components started

--- front-end/client/src/App.js
+++ front-end/client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import Quiz from "./views/Quiz";
+import Leaderboard from "./views/Leaderboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <nav style={{ display: "flex", gap: "1rem", padding: "1rem" }}>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+          <Link to="/quiz">Quiz</Link>
+          <Link to="/leaderboard">Leaderboard</Link>
+        </nav>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+        </Routes>
+      </Router>
   );
 }
 
